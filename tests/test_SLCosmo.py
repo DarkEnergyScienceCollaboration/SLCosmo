2a91f5b90bdbbb3245be640e7f66d27e591eb684
Phil Marshall <dr.phil.marshall@gmail.com>
2016-07-07 21:16:46 -0700

    Replaced run with read_in_time_delays

--- tests/test_SLCosmo.py
+++ tests/test_SLCosmo.py
@@ -11,14 +11,14 @@ class SLCosmoTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_run(self):
+    def test_read_in_time_delays(self):
         foo = desc.slcosmo.SLCosmo(self.message)
-        self.assertEquals(foo.run(), self.message)
+        self.assertEquals(foo.read_in_time_delays(), self.message)
 
     def test_failure(self):
         self.assertRaises(TypeError, desc.slcosmo.SLCosmo)
         foo = desc.slcosmo.SLCosmo(self.message)
-        self.assertRaises(RuntimeError, foo.run, True)
+        self.assertRaises(RuntimeError, foo.read_in_time_delays, True)
 
 if __name__ == '__main__':
     unittest.main()
