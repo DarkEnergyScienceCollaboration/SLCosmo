b84305c37774db27066b28865dca574e5b0b8418
Jim Chiang <jchiang@slac.stanford.edu>
2016-07-18 22:22:06 +0100

    add comments

--- tests/test_TDC2ensemble.py
+++ tests/test_TDC2ensemble.py
@@ -13,12 +13,23 @@ class TDC2ensembleTestCase(unittest.TestCase):
         pass
 
     def test_read_in_from(self):
+        """
+        Tests TDC2ensemble.read_in_from for 2-image and 4-image files.
+        """
+        # Tests for 2-image data.
         two_image = desc.slcosmo.TDC2ensemble.read_in_from(self.two_image_file)
         self.assertEqual(two_image.source, self.two_image_file)
-        self.assertEqual(two_image.Nsamples, 1000)
         self.assertEqual(two_image.Nimages, 2)
+
+        # Check for known sample size
+        self.assertEqual(two_image.Nsamples, 1000)
+
+        # Check for expected attributes for 2-image data.
+        # @todo: Think about refactoring how DeltaFP* are handled.
         self.assertNotEqual(two_image.DeltaFP, None)
         self.assertNotEqual(two_image.DeltaFP_err, None)
+
+        # Check that attributes for 4-image data are note present.
         self.assertRaises(AttributeError, lambda x: x.DeltaFP_AB, two_image)
         self.assertRaises(AttributeError, lambda x: x.DeltaFP_AB_err, two_image)
         self.assertRaises(AttributeError, lambda x: x.DeltaFP_AC, two_image)
@@ -26,16 +37,20 @@ class TDC2ensembleTestCase(unittest.TestCase):
         self.assertRaises(AttributeError, lambda x: x.DeltaFP_AD, two_image)
         self.assertRaises(AttributeError, lambda x: x.DeltaFP_AD_err, two_image)
 
+        # Tests for 4-image data.
         four_image = desc.slcosmo.TDC2ensemble.read_in_from(self.four_image_file)
         self.assertEqual(four_image.source, self.four_image_file)
         self.assertEqual(four_image.Nsamples, 1000)
         self.assertEqual(four_image.Nimages, 4)
+
+        # Test for expected attributes.
         self.assertNotEqual(four_image.DeltaFP_AB, None)
         self.assertNotEqual(four_image.DeltaFP_AB_err, None)
         self.assertNotEqual(four_image.DeltaFP_AC, None)
         self.assertNotEqual(four_image.DeltaFP_AC_err, None)
         self.assertNotEqual(four_image.DeltaFP_AD, None)
         self.assertNotEqual(four_image.DeltaFP_AD_err, None)
+
+        # Test that two-image attributes are not present.
         self.assertRaises(AttributeError, lambda x: x.DeltaFP, four_image)
         self.assertRaises(AttributeError, lambda x: x.DeltaFP_err, four_image)
-
